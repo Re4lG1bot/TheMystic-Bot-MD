c2de94fd3c0f3675359cec31bdfa31b4f6a0d3dd
Re4lG1bot <marcelinroodlenson@gmail.com>
2026-09-23 20:12:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ global.isBaileysFail = false;
 global.defaultLenguaje = 'es';
 
 global.owner = [
-  ['5219996125657', '👑 Propietario 👑', true],
+  ['18298460901', '👑 Propietario 👑', true],
   ['5492916450307'],
   ['5493794297363'],
   ['59169082575'],
